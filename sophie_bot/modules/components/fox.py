6e5f1bd1d2d11cc5b3b79c5b5ee7a8fbfd6d5b9a
MrYacha <YachaGorobtsov@gmail.com>
2019-06-23 15:46:02 +0300

    move flood_limit_dec,  get_strings_dec as t_flood_limit_dec and t_get_strings_dec

--- sophie_bot/modules/components/fox.py
+++ sophie_bot/modules/components/fox.py
@@ -6,7 +6,7 @@ import os
 from telethon import custom
 
 from sophie_bot import CONFIG, decorator, logger, tbot
-from sophie_bot.modules.helper_func.flood import flood_limit_dec
+from sophie_bot.modules.helper_func.flood import t_flood_limit_dec
 
 ftp_url = "ftp.orangefox.website"
 fox_groups = [483808054, -1001287179850, -1001280218923, -1001155400138, -1001362128194]
@@ -103,10 +103,10 @@ async def update_devices():
                 if changelog:
                     changelog += '\n'
                 changelog += "  " + str(owo)
-            changelog_file = f"{last_build[:-4]}.txt"
+            # changelog_file = f"{last_build[:-4]}.txt"
         else:
             changelog = None
-            changelog_file = None
+            # changelog_file = None
 
         DEVICES_STABLE[device] = {
             "codename": codename,
@@ -179,10 +179,10 @@ async def update_devices():
                 if changelog:
                     changelog += '\n'
                 changelog += "  " + str(owo)
-            changelog_file = f"{last_build[:-4]}.txt"
+            # changelog_file = f"{last_build[:-4]}.txt"
         else:
             changelog = None
-            changelog_file = None
+            # changelog_file = None
 
         DEVICES_BETA[device] = {
             "codename": codename,
@@ -247,7 +247,7 @@ async def do_update_devices(event):
 
 
 @decorator.command("list")
-@flood_limit_dec("list")
+@t_flood_limit_dec("list")
 async def list_stable(event):
     if event.chat_id in fox_beta_groups:
         text = "**Beta testing devices:**\n"
